b2df8b0ce4aeea3854516371affe35775301ed7f
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-04-21 11:44:04 +0300

    added FPS counter

---
+++ Assets/FPS.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour {
+
+    private float FPSvalue;
+    private Text FPStext;
+    private float deltaTime;
+
+    private float CD = 1.0f;
+    private float frames = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        FPStext = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTime += Time.deltaTime;
+        frames++;
+        if (deltaTime > CD)
+        {
+            FPSvalue = Mathf.Floor(frames / deltaTime);
+            frames = 0.0f;
+            deltaTime = 0.0f;
+            FPStext.text = "FPS:" + FPSvalue.ToString();
+        }
+	}
+}
